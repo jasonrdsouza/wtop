332ea48da5e487a42c380897ff0459e1f94ceff9
Jason Dsouza <jasonrdsouza@gmail.com>
2014-04-27 21:06:38 -0400

    Add todo list and other misc stuff

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 wtop
 ====
 
-A web version of top
+A web version of top.
+
+Forked off of: https://github.com/ddierickx/dtop
+
+
+Todo
+----
+- Change config files to YAML
+- remove gouuid dependancy
+- rearchitect
--- wtop.go
+++ wtop.go
@@ -10,7 +10,7 @@ import (
 	"os"
 )
 
-const VERSION = "0.1-SNAPSHOT"
+const VERSION = "0.1"
 
 var configFile = flag.String("c", "", "the location of the server configuration")
 var debug *bool = flag.Bool("d", false, "enable debug logging")
